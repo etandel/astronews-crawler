24f8f9cc60d8bd1fb0998de3ba638845c051c835
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2018-06-11 19:33:35 -0300

    Use async

--- crawl.py
+++ crawl.py
@@ -1,8 +1,8 @@
 # encoding: utf-8
 from collections import Counter
-from queue import LifoQueue
+import aiohttp
+import asyncio
 import re
-import requests
 import unicodedata
 from pprint import pprint
 
@@ -12,6 +12,11 @@ from typing import Callable, Mapping, Sequence
 
 MAX_DEPTH = 3
 KEY_WORDS = {'astron.+', 'astrof.+'}
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 
 def normalize(article: str) -> str:
@@ -45,40 +50,47 @@ def should_process(url):
 
 
 def should_visit(url):
-    return 'g1.globo.com' in href
+    return 'g1.globo.com' in url
 
 
-if __name__ == '__main__':
+async def crawl_loop(session, seed, keywords):
     counts = {}
-
-    q = LifoQueue()
     visited = set()
-    count_kws = keyword_counter(KEY_WORDS)
+    count_kws = keyword_counter(keywords)
 
-#    seed = 'http://g1.globo.com/ciencia-e-saude/'
-    seed = 'https://g1.globo.com/bemestar/noticia/toxoplasmose-entenda-os-sintomas-e-como-se-prevenir.ghtml'
-    q.put((0, seed))
+    q = asyncio.LifoQueue()
+    await q.put((0, seed))
+
     while not q.empty():
-        depth, url = q.get()
+        depth, url = await q.get()
         visited.add(url)
         print(url)
 
-        r = requests.get(url)
-        try:
-            r.raise_for_status()
-        except Exception:
-            pass
-        else:
-            parser = BeautifulSoup(r.content, 'lxml')
+        content = await fetch(session, url)
+        parser = BeautifulSoup(content, 'lxml')
 
-            if should_process(url):
-                article = get_g1_article_normalized(parser)
-                counts[url] = count_kws(article)
-                pprint(counts[url])
+        if should_process(url):
+            article = get_g1_article_normalized(parser)
+            counts[url] = count_kws(article)
+            pprint(counts[url])
 
-            if depth < MAX_DEPTH:
-                for link in parser.find_all('a'):
-                    href = link.get('href')
-                    if href and href not in visited and should_visit(href):
-                        q.put((depth + 1, href))
+        if depth < MAX_DEPTH:
+            for link in parser.find_all('a'):
+                href = link.get('href')
+                if href and href not in visited and should_visit(href):
+                    await q.put((depth + 1, href))
+    return counts
+
+
+async def main():
+#    seed = 'http://g1.globo.com/ciencia-e-saude/'
+    seed = 'https://g1.globo.com/bemestar/noticia/toxoplasmose-entenda-os-sintomas-e-como-se-prevenir.ghtml'
+
+    async with aiohttp.ClientSession() as session:
+        await crawl_loop(session, seed, KEY_WORDS)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
